024a0fe91135095b10a4b8607ed0209239ce1480
skywen_cp <skywen_cp@skywen-cpdeiMac-Pro.local>
2020-06-29 17:19:42 +0800

    首页板块管理

--- src/views/form/basicForm/index.vue
+++ src/views/form/basicForm/index.vue
@@ -1,124 +1,171 @@
 <template>
-  <page-header-wrapper content="表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。">
-    <a-card :body-style="{padding: '24px 32px'}" :bordered="false">
-      <a-form @submit="handleSubmit" :form="form">
-        <a-form-item
-          label="标题"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
-          <a-input
-            v-decorator="[
+  <page-header-wrapper content>
+    <a-row :gutter="24">
+      <a-col :span="12">
+        <a-card :body-style="{padding: '24px 32px'}" :bordered="true">
+          <a-tree
+            :tree-data="treeData"
+            show-icon
+            default-expand-all
+            :default-selected-keys="['0-0']"
+            @select="onSelect"
+          >
+            <a-icon slot="icon" type="carry-out" style="margin-right: 10px" />
+          </a-tree>
+        </a-card>
+      </a-col>
+      <a-col :span="12">
+        <a-card :body-style="{padding: '24px 32px'}" :bordered="true">
+          <a-form @submit="handleSubmit" :form="form">
+            <a-form-item
+              label="板块名称"
+              :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+              :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
+            >
+              <a-input
+                v-decorator="[
               'name',
-              {rules: [{ required: true, message: '请输入标题' }]}
+              {rules: [{ required: true, message: '请输入板块名称' }]}
             ]"
-            name="name"
-            placeholder="给目标起个名字" />
-        </a-form-item>
-        <a-form-item
-          label="起止日期"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
-          <a-range-picker
-            name="buildTime"
-            style="width: 100%"
-            v-decorator="[
-              'buildTime',
-              {rules: [{ required: true, message: '请选择起止日期' }]}
-            ]" />
-        </a-form-item>
-        <a-form-item
-          label="目标描述"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
-          <a-textarea
-            rows="4"
-            placeholder="请输入你阶段性工作目标"
-            v-decorator="[
-              'description',
-              {rules: [{ required: true, message: '请输入目标描述' }]}
-            ]" />
-        </a-form-item>
-        <a-form-item
-          label="衡量标准"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
-          <a-textarea
-            rows="4"
-            placeholder="请输入衡量标准"
-            v-decorator="[
+                name="name"
+                placeholder="请输入板块名称"
+              />
+            </a-form-item>
+            <a-form-item
+              label="类型"
+              :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+              :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
+            >
+              <a-select
+                placeholder="请选择类型"
+                v-decorator="[
               'type',
-              {rules: [{ required: true, message: '请输入衡量标准' }]}
-            ]" />
-        </a-form-item>
-        <a-form-item
-          label="客户"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }">
-          <a-input
-            placeholder="请描述你服务的客户，内部客户直接 @姓名／工号"
-            v-decorator="[
-              'customer',
-              {rules: [{ required: true, message: '请描述你服务的客户' }]}
-            ]" />
-        </a-form-item>
-        <a-form-item
-          label="邀评人"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
-          :required="false"
-        >
-          <a-input placeholder="请直接 @姓名／工号，最多可邀请 5 人" />
-        </a-form-item>
-        <a-form-item
-          label="权重"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
-          :required="false"
-        >
-          <a-input-number :min="0" :max="100" />
-          <span> %</span>
-        </a-form-item>
-        <a-form-item
-          label="目标公开"
-          :labelCol="{lg: {span: 7}, sm: {span: 7}}"
-          :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
-          :required="false"
-          help="客户、邀评人默认被分享"
-        >
-          <a-radio-group v-decorator="['target', { initialValue: 1 }]">
-            <a-radio :value="1">公开</a-radio>
-            <a-radio :value="2">部分公开</a-radio>
-            <a-radio :value="3">不公开</a-radio>
-          </a-radio-group>
-          <a-form-item v-show="form.getFieldValue('target') === 2">
-            <a-select mode="multiple">
-              <a-select-option value="4">同事一</a-select-option>
-              <a-select-option value="5">同事二</a-select-option>
-              <a-select-option value="6">同事三</a-select-option>
-            </a-select>
-          </a-form-item>
-        </a-form-item>
-        <a-form-item
-          :wrapperCol="{ span: 24 }"
-          style="text-align: center"
-        >
-          <a-button htmlType="submit" type="primary">提交</a-button>
-          <a-button style="margin-left: 8px">保存</a-button>
-        </a-form-item>
-      </a-form>
-    </a-card>
+              {rules: [{ required: true, message: '请选择类型' }]}
+            ]"
+              >
+                <a-select-option value="1">板块种类一</a-select-option>
+                <a-select-option value="2">板块种类二</a-select-option>
+                <a-select-option value="3">板块种类三</a-select-option>
+              </a-select>
+            </a-form-item>
+            <a-form-item
+              label="排序优先值"
+              :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+              :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
+            >
+              <a-input-number
+                :min="0"
+                v-decorator="[
+              'number',
+              {rules: [{ required: true, message: '请输入排序优先值' }]}
+            ]"
+              />
+            </a-form-item>
+            <a-form-item
+              label="最大容纳内容(商品)数"
+              :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+              :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
+            >
+              <a-input-number
+                :min="1"
+                v-decorator="[
+              'number',
+              {rules: [{ required: true, message: '请输入最大容纳内容(商品)数' }]}
+            ]"
+              />
+            </a-form-item>
+            <a-form-item
+              label="是否启用"
+              :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+              :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
+            >
+              <a-switch v-decorator="['switch', { valuePropName: 'checked' }]" />
+            </a-form-item>
+            <a-form-item
+              label="备注"
+              :labelCol="{lg: {span: 7}, sm: {span: 7}}"
+              :wrapperCol="{lg: {span: 10}, sm: {span: 17} }"
+            >
+              <a-textarea
+                rows="4"
+                placeholder="请输入备注"
+                v-decorator="[
+              'description',
+              {rules: [{ required: true, message: '请输入备注' }]}
+            ]"
+              />
+            </a-form-item>
+            <a-form-item :wrapperCol="{ span: 24 }" style="text-align: center">
+              <a-button htmlType="submit" type="primary">提交</a-button>
+            </a-form-item>
+          </a-form>
+        </a-card>
+      </a-col>
+    </a-row>
   </page-header-wrapper>
 </template>
 
 <script>
+import ATree from 'ant-design-vue/lib/tree'
+
+const treeData = [
+  {
+    title: '轮播图',
+    key: '0-0',
+    slots: {
+      icon: 'icon'
+    }
+  }, {
+    title: '活动板块',
+    key: '0-1',
+    slots: {
+      icon: 'icon'
+    }
+  }, {
+    title: '热门商品',
+    key: '0-2',
+    slots: {
+      icon: 'icon'
+    }
+  }, {
+    title: '官方精选',
+    key: '0-3',
+    slots: {
+      icon: 'icon'
+    }
+  }, {
+    title: '品牌周边',
+    key: '0-4',
+    slots: {
+      icon: 'icon'
+    }
+  }, {
+    title: '品牌精选',
+    key: '0-5',
+    slots: {
+      icon: 'icon'
+    }
+  }
+]
+
 export default {
   name: 'BaseForm',
+  components: {
+    ATree
+  },
   data () {
     return {
-      form: this.$form.createForm(this)
+      form: this.$form.createForm(this),
+      treeData
     }
   },
   methods: {
+    onSelect (selectedKeys, info) {
+      console.log('selected', selectedKeys, info)
+    },
+    onCheck (checkedKeys, info) {
+      console.log('onCheck', checkedKeys, info)
+    },
     // handler
     handleSubmit (e) {
       e.preventDefault()
--- src/views/home/IndexImages.vue
+++ src/views/home/IndexImages.vue
@@ -5,7 +5,7 @@
         <a-form layout="inline">
           <a-row :gutter="48">
             <a-col :md="8" :sm="24">
-              <a-form-item label="规则编号">
+              <a-form-item label="从所有数据中检索">
                 <a-input v-model="queryParam.id" placeholder=""/>
               </a-form-item>
             </a-col>
@@ -14,48 +14,14 @@
                 <a-select v-model="queryParam.status" placeholder="请选择" default-value="0">
                   <a-select-option value="0">全部</a-select-option>
                   <a-select-option value="1">关闭</a-select-option>
-                  <a-select-option value="2">运行中</a-select-option>
+                  <a-select-option value="2">开启</a-select-option>
                 </a-select>
               </a-form-item>
             </a-col>
-            <template v-if="advanced">
-              <a-col :md="8" :sm="24">
-                <a-form-item label="调用次数">
-                  <a-input-number v-model="queryParam.callNo" style="width: 100%"/>
-                </a-form-item>
-              </a-col>
-              <a-col :md="8" :sm="24">
-                <a-form-item label="更新日期">
-                  <a-date-picker v-model="queryParam.date" style="width: 100%" placeholder="请输入更新日期"/>
-                </a-form-item>
-              </a-col>
-              <a-col :md="8" :sm="24">
-                <a-form-item label="使用状态">
-                  <a-select v-model="queryParam.useStatus" placeholder="请选择" default-value="0">
-                    <a-select-option value="0">全部</a-select-option>
-                    <a-select-option value="1">关闭</a-select-option>
-                    <a-select-option value="2">运行中</a-select-option>
-                  </a-select>
-                </a-form-item>
-              </a-col>
-              <a-col :md="8" :sm="24">
-                <a-form-item label="使用状态">
-                  <a-select placeholder="请选择" default-value="0">
-                    <a-select-option value="0">全部</a-select-option>
-                    <a-select-option value="1">关闭</a-select-option>
-                    <a-select-option value="2">运行中</a-select-option>
-                  </a-select>
-                </a-form-item>
-              </a-col>
-            </template>
-            <a-col :md="!advanced && 8 || 24" :sm="24">
+            <a-col :md="8 || 24" :sm="24">
               <span class="table-page-search-submitButtons" :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">
                 <a-button type="primary" @click="$refs.table.refresh(true)">查询</a-button>
                 <a-button style="margin-left: 8px" @click="() => this.queryParam = {}">重置</a-button>
-                <a @click="toggleAdvanced" style="margin-left: 8px">
-                  {{ advanced ? '收起' : '展开' }}
-                  <a-icon :type="advanced ? 'up' : 'down'"/>
-                </a>
               </span>
             </a-col>
           </a-row>
@@ -63,7 +29,7 @@
       </div>
 
       <div class="table-operator">
-        <a-button type="primary" icon="plus" @click="handleAdd">新建</a-button>
+        <a-button type="primary" icon="plus" @click="handleAdd">添加轮播图</a-button>
         <a-dropdown v-action:edit v-if="selectedRowKeys.length > 0">
           <a-menu slot="overlay">
             <a-menu-item key="1"><a-icon type="delete" />删除</a-menu-item>
@@ -132,20 +98,20 @@ const columns = [
     scopedSlots: { customRender: 'serial' }
   },
   {
-    title: '规则编号',
+    title: '类型',
     dataIndex: 'no'
   },
   {
-    title: '描述',
-    dataIndex: 'description',
-    scopedSlots: { customRender: 'description' }
+    title: '图片1',
+    dataIndex: 'image1',
+    scopedSlots: { customRender: 'image1' }
   },
   {
-    title: '服务调用次数',
+    title: '跳转链接',
     dataIndex: 'callNo',
     sorter: true,
     needTotal: true,
-    customRender: (text) => text + ' 次'
+    customRender: (text) => text
   },
   {
     title: '状态',
